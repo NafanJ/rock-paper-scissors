8c8deb551c35ff0683935852bc14f136c05dff03
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-10-13 12:19:14 +0100

    Win Conditions
    Added a counter for both player and computer to track wins and determine the ultimate winner

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const possibleChoices = ["rock", "paper", "scissors"]
+let playerWins = 0;
+let computerWins = 0;
 
 function getComputerChoice(){
-    const randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+    let randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+    randomChoice = 'rock';
     return randomChoice;
 }
 
@@ -25,26 +28,32 @@ function playRound(playerSelection, computerSelection){
     } else if (playerSelection === "rock") {
         if (computerSelection === "scissors"){
             console.log("You win! Rock beats Scissors")
+            playerWins++;
         } else {
             console.log("You lose! Paper beats rock")
+            computerWins++;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock"){
             console.log("You win! Paper beats rock")
+            playerWins++;
         } else {
             console.log("You lose! Scissors beats paper")
+            computerWins++;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper"){
             console.log("You win! Scissors beats paper")
+            playerWins++;
         } else {
             console.log("You lose! Rock beats scissors")
+            computerWins++;
         }
     }
 }
 
 function game(){
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; (playerWins < 5)||(computerWins < 5); i++){
         console.log(`Round ${i + 1}`)
         playRound(getPlayerChoice(), getComputerChoice());
     }
